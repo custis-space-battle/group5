efe9b730afeb030d1f70aa6a416558e30c5b9ae3
Участник <mitroshin.develop@gmail.com>
2017-05-20 14:27:27 +0300

    create sendMessage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  * Created by Участник on 20.05.2017.
  */
@@ -6,6 +9,16 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         RabbitConn.connect();
-        RabbitConn.sendMessage("start:bot");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Boolean isExist = true;
+        while (isExist) {
+            String temp = reader.readLine();
+            if (temp.equals("exit")) {
+                isExist = false;
+            } else {
+                RabbitConn.sendMessage(temp);
+            }
+        }
     }
 }

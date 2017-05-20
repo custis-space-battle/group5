6beb8379bd6bd2a44a6550764a3e91991ba0a710
ganievart <ganievart@gmail.com>
2017-05-20 14:55:08 +0300

    hit random

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,24 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Random;
 
 /**
  * Created by Участник on 20.05.2017.
  */
 public class Main {
+
+    public static String hit() {
+
+        Random random = new Random();
+        int x = random.nextInt(10) + 1;
+        int y = random.nextInt(10) + 1;
+
+
+        System.out.println(x + " " + y);
+
+        return null;
+
+    }
 
     public static void main(String[] args) throws Exception {
 
@@ -12,13 +26,19 @@ public class Main {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Boolean isExist = true;
-        while (isExist) {
+        while (true) {
             String temp = reader.readLine();
             if (temp.equals("exit")) {
-                isExist = false;
+                break;
             } else {
+                if (temp.contains("fire")) {
+                    while (true) hit();
+                }
                 RabbitConn.sendMessage(temp);
             }
         }
+
+
+        reader.close();
     }
 }
